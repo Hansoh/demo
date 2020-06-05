173a685fa1fa5c0fb72790c34a8b326a63cee3f8
Huan Hans <jan.strzelecki@wir.pl>
2020-06-05 14:18:55 +0200

    Initial commit

---
+++ src/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.w3c.dom.ls.LSInput;
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("pubceweff");
+    }
+
+
+
+}
